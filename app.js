ceb38308fae1c1ebbd049e3bbd493dec989d020d
Tao Ning <ningtaous@gmail.com>
2016-11-04 14:24:39 -0400

    [refactor] Move ember frontend to dist folder
    Now public folder has a clearer structure.
    	- about/ includes about page, which is based on canvas template
    	- dist/ includes build folder from ember

--- app.js
+++ app.js
@@ -18,16 +18,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/dist')));
 
 app.get('/about', function(req, res) {
   res.sendfile('./public/about/about.html');
 })
 
 app.get('*', function(req, res) {
-  res.sendfile('./public/index.html');
+  res.sendfile('./public/dist/index.html');
 })
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
